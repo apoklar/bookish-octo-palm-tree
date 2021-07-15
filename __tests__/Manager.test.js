758584392ccea06fd4beffe41240cb65c1e78b03
andrp <a.poklar@yahoo.com>
2021-07-14 20:14:29 -0500

    added test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,19 @@
 const Manager = require('../lib/Manager');
+
+test("Can get office number", () => {
+    const testValue = "officeNumber";
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+  
+test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Foo", 1, "test@test.com", "officeNumber");
+    expect(e.getRole()).toBe(testValue);
+});
+  
+test("Can get officeNumber via getofficeNumber()", () => {
+    const testValue = "officeNumber";
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
